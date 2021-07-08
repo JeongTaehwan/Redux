ca2aac17d24a0318ca39449bd8493e7be5bf17ce
Tapuwan <68889258+JeongTaehwan@users.noreply.github.com>
2021-07-08 20:47:53 +0900

    [ADD] useSelector 최적화

--- src/container/CounterContainer.js
+++ src/container/CounterContainer.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import Counter from '../components/Counter';
-import { useSelector, useDispatch } from 'react-redux'; // useSelector는 상태를 조회할 때 사용
+import { useSelector, useDispatch, shallowEqual } from 'react-redux'; // useSelector는 상태를 조회할 때 사용
 import { decrease, increase, setDiff } from '../modules/counter';
 
 function CounterContainer() {
     const { number, diff } = useSelector(state => ({
         number: state.counter.number,
         diff: state.counter.diff,
-    }));
+    }),
+        shallowEqual
+    );
+    // const number = useSelector(state => state.counter.number);
+    // const diff = useSelector(state => state.counter.diff);
 
     const dispatch = useDispatch();
 
